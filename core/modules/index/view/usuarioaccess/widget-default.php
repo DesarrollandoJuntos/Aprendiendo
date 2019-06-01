a2c9dd760057c20791430a9b638525f7f50c7927
miguel angel lozano jimenez <youmartinez2016@gmail.com>
2019-06-01 14:57:40 -0500

    Vistas de Usuarios

--- core/modules/index/view/usuarioaccess/widget-default.php
+++ core/modules/index/view/usuarioaccess/widget-default.php
@@ -23,7 +23,7 @@
   </div>
   <div class="form-group">
     <div class="col-lg-offset-2 col-lg-10">
-      <button type="submit" class="btn btn-block btn-default">Registrarme</button>
+      <button type="submit" class="btn btn-block btn-default">Iniciar Sesion</button>
     </div>
   </div>
 </form>
