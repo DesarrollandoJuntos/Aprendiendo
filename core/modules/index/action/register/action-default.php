5a0562069ac22e4af715bab6521cc1a6343404d9
miguel angel lozano jimenez <youmartinez2016@gmail.com>
2019-06-01 17:38:46 -0500

    Agregando archivos

--- core/modules/index/action/register/action-default.php
+++ core/modules/index/action/register/action-default.php
@@ -1,36 +1,19 @@
-
 <?php
 
-if(!isset($_SESSION["client_id"])) {
-$user = $_POST['email'];
-$pass = sha1(md5($_POST['password']));
+if(isset($_POST["accept"])){
+$c= ClientData::getByEmail($_POST["email"]);
+if($c==null){
+$client =  new ClientData();
+$client->email = $_POST["email"];
+$client->password = sha1(md5($_POST['password']));
 
-$base = new Database();
-$con = $base->connect();
- $sql = "select * from client where (email= \"".$user."\") and password= \"".$pass."\" and is_active=1";
-//print $sql;
-$query = $con->query($sql);
-$found = false;
-$userid = null;
-while($r = $query->fetch_array()){
-	$found = true ;
-	$userid = $r['id'];
-}
+$client->add();
 
-if($found==true) {
-//	session_start();
-//	print $userid;
-	$_SESSION['client_id']=$userid ;
-//	setcookie('userid',$userid);
-//	print $_SESSION['userid'];
-	print "Cargando ... $user";
-	Core::redir("index.php?view=clientaccess");
-}else {
-	Core::redir("index.php?view=clientaccess");
-}
-
+Core::redir("index.php?view=clientaccess");
 }else{
-	Core::redir("index.php?view=clientaccess");
-	
+Core::alert("Ya existe un usuario registrado con esta direccion email.");
+Core::redir("./?view=register");
+
+}
 }
 ?>
