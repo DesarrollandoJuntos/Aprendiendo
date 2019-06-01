3fa6674285024fb643fd468eaf2dc05766e02e69
miguel angel lozano jimenez <youmartinez2016@gmail.com>
2019-06-01 17:21:14 -0500

    Agregando archivos

--- core/modules/index/action/register/action-default.php
+++ core/modules/index/action/register/action-default.php
@@ -8,7 +8,7 @@ $client->name = $_POST["name"];
 $client->lastname = $_POST["lastname"];
 $client->email = $_POST["email"];
 $client->address = $_POST["address"];
-$client->password = crypt($_POST["password"]);
+$client->password = sha1(md5($_POST['password']));
 $client->phone = $_POST["phone"];
 $client->add();
 
