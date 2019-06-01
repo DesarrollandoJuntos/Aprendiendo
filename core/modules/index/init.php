0e3e9aabad85fe6e7567754825bc28f45d4058c1
miguel angel lozano jimenez <youmartinez2016@gmail.com>
2019-06-01 13:53:07 -0500

    Iniciando del sistema

--- core/modules/index/init.php
+++ core/modules/index/init.php
@@ -1,14 +1,10 @@
 <?php
 
-
-
-
 if(!isset($_GET["action"])){
-//	Bootload::load("default");
+
 	Module::loadLayout();
 }else{
-///	echo "d:";
-	// $params = $_REQUEST;
+
 	Action::load($_GET["action"],new Request());
 }
 
